bdee11617163d6fee4d0a2ec4b966428fe2a7a29
sjsjsj1246 <sjsjsj1246@gmail.com>
2022-07-20 07:45:46 +0900

    Feat: 화상 채팅 기능 구현

--- lib/screens/camScreen.dart
+++ lib/screens/camScreen.dart
@@ -1,5 +1,9 @@
+import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rtc/const/agora.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
+import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
 
 class CamScreen extends StatefulWidget {
   const CamScreen({Key? key}) : super(key: key);
@@ -9,6 +13,10 @@ class CamScreen extends StatefulWidget {
 }
 
 class _CamScreenState extends State<CamScreen> {
+  RtcEngine? engine;
+  int? uid;
+  int? otherUid;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,9 +34,54 @@ class _CamScreenState extends State<CamScreen> {
               return Center(child: CircularProgressIndicator());
             }
 
-            return Center(child: Text("권한이 있습니다."));
+            return Column(
+              children: [
+                Expanded(
+                    child: Stack(children: [
+                  renderMainView(),
+                  Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                          color: Colors.grey,
+                          height: 160,
+                          width: 120,
+                          child: renderSubView()))
+                ])),
+                ElevatedButton(
+                  onPressed: () {
+                    if (engine != null) {
+                      engine?.leaveChannel();
+                    }
+
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('채널 나가기'),
+                ),
+              ],
+            );
           }),
     );
+  }
+
+  Widget renderSubView() {
+    if (otherUid == null) {
+      return Center(
+        child: Text("채널에 유저가 없습니다."),
+      );
+    } else {
+      return RtcRemoteView.SurfaceView(
+        uid: otherUid!,
+        channelId: CHANNEL_NAME,
+      );
+    }
+  }
+
+  Widget renderMainView() {
+    if (uid == null) {
+      return Center(child: Text("채널에 참여해주세요"));
+    } else {
+      return RtcLocalView.SurfaceView();
+    }
   }
 
   Future<bool> init() async {
@@ -37,11 +90,44 @@ class _CamScreenState extends State<CamScreen> {
     final cameraPermission = resp[Permission.camera];
     final micPermission = resp[Permission.microphone];
 
+    if (engine == null) {
+      RtcEngineContext context = RtcEngineContext(APP_ID);
+
+      engine = await RtcEngine.createWithContext(context);
+
+      engine!.setEventHandler(RtcEngineEventHandler(
+          joinChannelSuccess: (String channel, int uid, int elapsed) {
+        print('joinChannelSuccess: $channel, uid: $uid, elapsed: $elapsed');
+        setState(() {
+          this.uid = uid;
+        });
+      }, userJoined: (int uid, int elapsed) {
+        print('userJoined: $uid, elapsed: $elapsed');
+        setState(() {
+          this.otherUid = uid;
+        });
+      }, leaveChannel: (state) {
+        print("채널에서 나갔습니다.");
+        setState(() {
+          uid = null;
+        });
+      }, userOffline: (int uid, UserOfflineReason reason) {
+        print("상대가 채널에서 나갔습니다");
+        setState(() {
+          otherUid = null;
+        });
+      }));
+
+      await engine!.enableVideo();
+
+      await engine!.joinChannel(TEMP_TOKEN, CHANNEL_NAME, null, 0);
+    }
+
     if (cameraPermission != PermissionStatus.granted ||
         micPermission != PermissionStatus.granted) {
-      return false;
-    } else {
-      return true;
+      throw '카메라 또는 마이크 권한이 없습니다.';
     }
+
+    return true;
   }
 }
